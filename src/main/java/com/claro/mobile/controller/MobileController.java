b88afff64dd8f88385fd3400cec2274362bba865
Jonnes Sena <jonnes-sena@hotmail.com>
2018-02-28 21:23:12 -0300

    ultimas atualizações

--- src/main/java/com/claro/mobile/controller/MobileController.java
+++ src/main/java/com/claro/mobile/controller/MobileController.java
@@ -6,11 +6,11 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.claro.mobile.model.MobileDTO;
@@ -21,7 +21,8 @@ public class MobileController {
 	@Autowired
 	IMobileService service;
 	Logger log;
-
+	
+	@CrossOrigin
 	@RequestMapping(value = "/claro/mobile", method = RequestMethod.GET)
 	public List<MobileDTO> mobiles() throws Exception {
 		try {
@@ -31,7 +32,8 @@ public class MobileController {
 		}
 		return null;
 	}
-
+	
+	@CrossOrigin
 	@RequestMapping(value = "/claro/mobile", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<MobileDTO> mobile(@RequestBody MobileDTO input) throws Exception {
 		try {
@@ -44,7 +46,6 @@ public class MobileController {
 	}
 
 	@RequestMapping(value = "/claro/mobile/{code}", method = RequestMethod.GET)
-//	@ResponseBody
 	public MobileDTO find(@PathVariable("code") long code) throws Exception {
 		try {
 			return service.find(code);
